18ef39724ffd21527340dff8f7a071a17591f4a9
nanoni17728 <nanoni@riseup.net>
2023-06-09 11:12:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nano's CHIP-8
+
+Learning to make a runtime in C++. Modern (not a spec in mind yet) and Functional minded.
+
+## Licence
+This is free and unencumbered software released into the public domain. Use
+it however you want, check the [UNLISENCE](UNLISENCE.md) file for more
+informations.
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "CHIP-8 runtime in modern C++";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.clang
+        ];
+        buildInputs = [
+          pkgs.lldb
+        ];
+        shellHook = ''
+          if command -v > /dev/null
+            then fish
+          fi
+        '';
+      };
+    });
+}
